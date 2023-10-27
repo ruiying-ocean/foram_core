ea7e42d27701f5c6f665474a3d4458b902881143
Rui Ying (Mac) <38958822+ruiying-ocean@users.noreply.github.com>
2023-10-27 10:27:41 +0100

    file: update histogram

--- code/sample_efforts.py
+++ code/sample_efforts.py
@@ -9,6 +9,7 @@ def pd_to_gpd(data):
     geometry = [Point(xy) for xy in zip(data['Longitude'], data['Latitude'])]
     gdf = gpd.GeoDataFrame(data, geometry=geometry, crs="EPSG:4326")
     return gdf
+from matplotlib.gridspec import GridSpec
 
 def main():
     "plot sample number over 1 degree bin (x axis)"
@@ -23,16 +24,20 @@ def main():
     pi_color = sns.color_palette("Set2")[1]
     lgm_color = sns.color_palette("Set2")[0]
 
-    fig = plt.figure(figsize=(10, 8), tight_layout=True)
-    gs = fig.add_gridspec(4, 5)
+    fig = plt.figure(figsize=(10.5, 8), tight_layout=True)
 
-    # Add the world map to the first subplot
-    ax_map1 = fig.add_subplot(gs[0:2, :4])
-    ax_map2 = fig.add_subplot(gs[2:4, :4])
-    ax_hs1 = fig.add_subplot(gs[0, 4:])
-    ax_hs2 = fig.add_subplot(gs[1, 4:])
-    ax_hs3 = fig.add_subplot(gs[2, 4:])
-    ax_hs4 = fig.add_subplot(gs[3, 4:])
+    # Create a GridSpec with 4 rows and 7 columns
+    ## 5:2 for map to histogram width ratio
+    gs = GridSpec(4, 7, figure=fig)
+
+    # Define the subplots
+    ax_map1 = plt.subplot(gs[0:2, :5])
+    ax_map2 = plt.subplot(gs[2:4, :5])
+    ax_hs1 = plt.subplot(gs[0, 5:])
+    ax_hs2 = plt.subplot(gs[1, 5:])
+    ax_hs3 = plt.subplot(gs[2, 5:])
+    ax_hs4 = plt.subplot(gs[3, 5:])
+
 
     ax_hs = [ax_hs1, ax_hs2, ax_hs3, ax_hs4]
     all_axs = [ax_map1, ax_map2, ax_hs1, ax_hs2, ax_hs3, ax_hs4]
@@ -51,10 +56,22 @@ def main():
     for i, col in enumerate(cols):
         ax_hs[i].hist(pi[col], bins=30, label='PI', color=pi_color)
         ax_hs[i].hist(lgm[col], bins=30, label='LGM', color=lgm_color)
+
+        ## get the location of highest bar of pi and normalise to 0-1
+        max_x = max(ax_hs[i].patches[:30], key=lambda p: p.get_height()).get_x()
+        max_y = max(ax_hs[i].get_yticks())
+
+        ax_hs[i].set_ylim(0, max_y*0.33)
+
+        ## add annotation for cutted off bars
+        annotation = 'highest: '+str(int(max_y))
+
         if col != 'SST':
             ax_hs[i].set_xlabel(col+' Abundance')
+            ax_hs[i].annotate(annotation, xy=(0.15, 0.85), xycoords='axes fraction', fontsize=8)
         else:
             ax_hs[i].set_xlabel(col+' (°C)')
+            ax_hs[i].annotate(annotation, xy=(0.4, 0.85), xycoords='axes fraction', fontsize=8)
         ax_hs[i].set_ylabel('Sample number')
         ax_hs[i].legend()
 
