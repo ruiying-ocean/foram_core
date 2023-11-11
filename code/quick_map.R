2247ce2209fe6b34a4fd604c183b738361a78f7c
Rui Ying (Mac) <38958822+ruiying-ocean@users.noreply.github.com>
2023-11-11 00:48:36 +0000

    update quick_map.R

--- code/quick_map.R
+++ code/quick_map.R
@@ -4,11 +4,11 @@ library(rnaturalearth)
 # Create a ggplot object with the land and ocean mask
 quick_map <- function(data, lon, lat){
   ## if data not exist, then download
-  if (!file.exists("world_map.rds")) {
+  if (!file.exists("code/world_map.rds")) {
     world <- ne_download(scale = "small", type = "land", category = "physical", returnclass = "sf")
     saveRDS(world, "world_map.rds")
   } else {
-    world <- readRDS("world_map.rds")
+    world <- readRDS("code/world_map.rds")
   }
   
   p <- ggplot() +
@@ -19,6 +19,5 @@ quick_map <- function(data, lon, lat){
 }
 
 ## example
-quick_map(climap_site, 'Long', 'Lat')
-read_csv("raw/CLIMAP/formatted_site_info.csv") %>% quick_map('Longitude', 'Latitude')
+read_csv("fg/lgm_margo_fg_r.csv") %>% quick_map('Longitude', 'Latitude')
 ## read_csv("raw/CLIMAP/CLIMAP_FORMATTED.csv") %>% quick_map('Longitude', 'Latitude')
