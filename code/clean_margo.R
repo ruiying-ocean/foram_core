3a7838f4fa93ea6ce6259630fda6cb636f921eb9
Rui Ying (Mac) <38958822+ruiying-ocean@users.noreply.github.com>
2023-11-14 19:26:21 +0000

    more accurate abs/relative abundance in MARGO now
    I manually label the species data type (as 1,2,3) [last commit],
    with comments in related scripts.
    Also change the code and manually check the output.

--- code/clean_margo.R
+++ code/clean_margo.R
@@ -1,14 +1,22 @@
 ## MARGO contains both relative and absolute abundance
 ## North Atlantic has only relative abundance
+
+## manually labelled datatype
+## Datatype
+## 1: species in relative, total in relative
+## 2: species in absolute, total in absolute
+## 3: species in relative, total in absolute
+## 4: species in absolute, total in relative
+
 n_atlantic <- read_csv("raw/MARGO/clean format/LGM_MARGO_NAtlantic.csv")
 s_atlantic <- read_csv("raw/MARGO/clean format/LGM_MARGO_SAtlantic.csv")
 indopac <- read_csv("raw/MARGO/clean format/LGM_MARGO_IndoPacific.csv")
-pacific <- read_csv("raw/MARGO/clean format/LGM_MARGO_Pacific.csv")
+pacific <- readxl::read_xlsx("raw/MARGO/clean format/LGM_MARGO_Pacific.xlsx")
 med <- read_csv("raw/MARGO/clean format/LGM_MARGO_Med.csv")
 ## fill the column of sample depth
 med <- med %>% mutate(`Sample depth - upper (m)` = `Sample depth - lower (m)`)
 ## only for convenience when select species
-pacific <- pacific %>% mutate(`Globigerinita uvula` = 0, .after=`Globigerinita glutinata`)
+pacific <- pacific %>% mutate(`Globigerinita uvula` = 0, .after = `Globigerinita glutinata`)
 
 ## -----------------------------------
 ## Clean Species Name
@@ -22,34 +30,35 @@ remove_columns <- function(data, column_names) {
   return(data)
 }
 
-clean_sp_name <- function(data){
-  
-    data <- data %>% dplyr::filter(!is.na(Longitude) & !is.na(Latitude))
-    data <- data %>% remove_columns(c("Globigerinoides ruber total", 
-                               "Berggrenia pumilio + T. humilis",
-                               "Globorotalia menardii + tumida",
-                               "P/D integrade + N. pachyderma R",
-                               "Globigerinoides sacculifer w/o sac",
-                               "Globigerinoides sacculifer with sac",
-                               "Globorotalia truncatulinoides L",
-                               "Globorotalia truncatulinoides R", 
-                               "Other UnID","Other ID"))
+clean_sp_name <- function(data) {
+  data <- data %>% dplyr::filter(!is.na(Longitude) & !is.na(Latitude))
+  data <- data %>% remove_columns(c(
+    "Globigerinoides ruber total",
+    "Berggrenia pumilio + T. humilis",
+    "Globorotalia menardii + tumida",
+    "P/D integrade + N. pachyderma R",
+    "Globigerinoides sacculifer w/o sac",
+    "Globigerinoides sacculifer with sac",
+    "Globorotalia truncatulinoides L",
+    "Globorotalia truncatulinoides R",
+    "Other UnID", "Other ID"
+  ))
 
-    data <- data %>% replace_column_name("Beela digitata", "Globigerinella digitata")
-    data <- data %>% replace_column_name("Globigerinoides sacc", "Trilobatus sacculifer")
-    data <- data %>% replace_column_name("Globigerinoides sacc total", "Trilobatus sacculifer")
-    data <- data %>% replace_column_name("Dentagloborotalia anfracta", "Dentigloborotalia anfracta")
-    data <- data %>% replace_column_name("Neogloboquadrina pachyderma L", "Neogloboquadrina pachyderma")
-    data <- data %>% replace_column_name("Neogloboquadrina pachyderma R", "Neogloboquadrina incompta")
-    data <- data %>% replace_column_name("Globorotalia crassula","Globorotalia crassa")
-    data <- data %>% replace_column_name("Globorotalia menardii flexuosa", "Globorotalia tumida")
-    data <- data %>% replace_column_name("Globigerinoides ruber (white)", "Globigerinoides ruber albus")
-    data <- data %>% replace_column_name("Globigerinoides ruber (pink)", "Globigerinoides ruber ruber")
-    data <- data %>% replace_column_name("Globigerinella siphonifera (=aequilateralis)", "Globigerinella siphonifera")
-    data <- data %>% replace_column_name("Globorotalia truncatulinoides total", "Globorotalia truncatulinoides")
-    data <- data %>% replace_column_name("Tenuitella iota", "Tenuitellita iota")
+  data <- data %>% replace_column_name("Beela digitata", "Globigerinella digitata")
+  data <- data %>% replace_column_name("Globigerinoides sacc", "Trilobatus sacculifer")
+  data <- data %>% replace_column_name("Globigerinoides sacc total", "Trilobatus sacculifer")
+  data <- data %>% replace_column_name("Dentagloborotalia anfracta", "Dentigloborotalia anfracta")
+  data <- data %>% replace_column_name("Neogloboquadrina pachyderma L", "Neogloboquadrina pachyderma")
+  data <- data %>% replace_column_name("Neogloboquadrina pachyderma R", "Neogloboquadrina incompta")
+  data <- data %>% replace_column_name("Globorotalia crassula", "Globorotalia crassa")
+  data <- data %>% replace_column_name("Globorotalia menardii flexuosa", "Globorotalia tumida")
+  data <- data %>% replace_column_name("Globigerinoides ruber (white)", "Globigerinoides ruber albus")
+  data <- data %>% replace_column_name("Globigerinoides ruber (pink)", "Globigerinoides ruber ruber")
+  data <- data %>% replace_column_name("Globigerinella siphonifera (=aequilateralis)", "Globigerinella siphonifera")
+  data <- data %>% replace_column_name("Globorotalia truncatulinoides total", "Globorotalia truncatulinoides")
+  data <- data %>% replace_column_name("Tenuitella iota", "Tenuitellita iota")
 
-    return(data)
+  return(data)
 }
 
 n_atlantic <- clean_sp_name(n_atlantic)
@@ -63,73 +72,90 @@ med <- clean_sp_name(med)
 ## -----------------------------------
 
 ## convert all data into relative abundance
-only_relative_abundance <- function(data){
-    ## 1. filter relative and absolute abundance
-    raw_data <- data %>% dplyr::filter(`%(1) or Raw(2)` == 2)
-    perc_data <- data %>% dplyr::filter(`%(1) or Raw(2)` == 1)
-    ## 2 convert absolute abundance to relative abundance
-    raw_data <- raw_data %>% mutate_at(vars(`Orbulina universa`:`Globigerinita uvula`),
-                                       ~ . / `Total Planktics` * 100)
-    ## 3 merge together
-    new_data <- rbind(raw_data, perc_data)
-    ## 4 remove the last column, it'll be unuseful
-    new_data <- new_data %>% remove_columns(c("%(1) or Raw(2)", "Total Planktics" ))
-    ## 5 convert relative abundance in decimal format
-    new_data <- new_data %>% mutate_at(vars(`Orbulina universa`:`Globigerinita uvula`),
-                                     ~ . / 100)
+only_relative_abundance <- function(data) {
+  
+    perc_data <- data %>% dplyr::filter(`Datatype` == 1)
+    raw_data <- data %>% dplyr::filter(`Datatype` == 2)
+    fake_raw_data <- data %>% dplyr::filter(`Datatype` == 3)
+    
+  ## 2 convert absolute abundance to relative abundance
+  raw_data <- raw_data %>% mutate_at(
+    vars(`Orbulina universa`:`Globigerinita uvula`),
+    ~ . / `Total Planktics` * 100
+    )
+    
+  ## 3 merge together
+    new_data <- rbind(perc_data, raw_data, fake_raw_data)
+    
+  ## 4 remove the last column, it'll be unuseful
+  new_data <- new_data %>% remove_columns(c("Datatype", "Total Planktics"))
+  ## 5 convert relative abundance in decimal format
+  new_data <- new_data %>% mutate_at(
+    vars(`Orbulina universa`:`Globigerinita uvula`),
+    ~ . / 100
+  )
 
-    return(new_data)    
+  return(new_data)
 }
 
 ## convert all data into abs abundance
-only_abs_abundance <- function(data){
+only_abs_abundance <- function(data) {
   
-    ## keep absolute abundance and process perc data
-    raw_data <- data %>% dplyr::filter(`%(1) or Raw(2)` == 2)
-    ## if the total planktics variable is not "around" 100, then mutiply species column by this column
-    ## i.e., convert to absolute abundance
-    perc_data <- data %>% dplyr::filter(`%(1) or Raw(2)` == 1)
+  raw_data <- data %>% dplyr::filter(`Datatype` == 2)
+    perc_data <- data %>% dplyr::filter(`Datatype` == 1)
+    fake_raw_data <- data %>% dplyr::filter(`Datatype` == 3)
+
+    ## convert fake raw data to real raw data
+    fake_raw_data <- fake_raw_data %>% mutate_at(
+      vars(`Orbulina universa`:`Globigerinita uvula`),
+      ~ ceiling(. * `Total Planktics` / 100)
+    )
+ 
+  
+  perc_data <- perc_data %>%
+      drop_na(`Total Planktics`)
     
-    threshold <- 5 
-    perc_data <- perc_data %>%  mutate_at(vars(`Orbulina universa`:`Globigerinita uvula`),
-                        ~ if_else(abs(`Total Planktics` - 100) > threshold & !is.na(`Total Planktics`),
-                                  as.integer(. * `Total Planktics`/100),
-                                  NA))
-    perc_data <- perc_data %>% drop_na(`Total Planktics`)%>% drop_na(`Orbulina universa`)
-    new_data <- rbind(raw_data, perc_data)
-    
-    new_data <- new_data %>% remove_columns(c("%(1) or Raw(2)", "Total Planktics" ))
-    return(new_data)
+  new_data <- rbind(raw_data, fake_raw_data)
+
+  new_data <- new_data %>% remove_columns(c("Datatype", "Total Planktics"))
+  return(new_data)
 }
 
-combine_data_frames <- function(..., wide_format=FALSE) {
+combine_data_frames <- function(..., wide_format = FALSE) {
   # Combine all input data frames into a single list
   data_frames <- list(...)
-  
+
   # Pivot longer on each data frame
   long_data_frames <- lapply(data_frames, function(df) {
-    df <- df %>% remove_columns(c("sedimentation rate (cm/ky)",
-                         "added by (name)", "Species",
-                         "calendar age estimate (cal ky BP)","Instrument",
-                         "Chronostratigraphic quality (chronozone level)",
-                         "Laboratory", "Sample depth - lower (m)",
-                         "date of addition","Publication"))
-    
-  pivot_longer(df, cols =  `Orbulina universa`:`Globigerinita uvula`,
-                 names_to = "Species", values_to = "Abundance")
+    df <- df %>% remove_columns(c(
+      "sedimentation rate (cm/ky)",
+      "added by (name)", "Species",
+      "calendar age estimate (cal ky BP)", "Instrument",
+      "Chronostratigraphic quality (chronozone level)",
+      "Laboratory", "Sample depth - lower (m)",
+      "date of addition", "Publication"
+    ))
+
+    pivot_longer(df,
+      cols = `Orbulina universa`:`Globigerinita uvula`,
+      names_to = "Species", values_to = "Abundance"
+    )
   })
-  
+
   # Combine the long data frames into a single data frame
   combined_data <- do.call(rbind, long_data_frames)
-  
+
   # Pivot wider to reshape the combined data frame
   # if identifier is not unique, then take average
   if (wide_format) {
-    wide_data <- combined_data %>% distinct() %>%
-      pivot_wider(names_from = "Species", values_from = "Abundance",
-                  values_fn = mean)
+    wide_data <- combined_data %>%
+      distinct() %>%
+      pivot_wider(
+        names_from = "Species", values_from = "Abundance",
+        values_fn = mean
+      )
     return(wide_data)
-  } else{
+  } else {
     return(combined_data)
   }
 }
@@ -148,42 +174,55 @@ s_atlantic_a <- only_abs_abundance(s_atlantic)
 n_atlantic_a <- only_abs_abundance(n_atlantic)
 indopac_a <- only_abs_abundance(indopac)
 
-margo_a <- combine_data_frames(s_atlantic_a, n_atlantic_a, pacific_a,indopac_a,med_a) %>%
-  rename("Absolute Abundance"="Abundance") %>% rowwise()%>%
+margo_a <- combine_data_frames(s_atlantic_a, n_atlantic_a, pacific_a, indopac_a, med_a) %>%
+  rename("Absolute Abundance" = "Abundance") %>%
+  rowwise() %>%
   mutate_at(.vars = "Species", .funs = species_abbrev)
 
-margo_r <- combine_data_frames(s_atlantic_r, n_atlantic_r, pacific_r, indopac_r,med_r) %>%
-  rename("Relative Abundance"="Abundance")%>% rowwise()%>%
+margo_r <- combine_data_frames(s_atlantic_r, n_atlantic_r, pacific_r, indopac_r, med_r) %>%
+  rename("Relative Abundance" = "Abundance") %>%
+  rowwise() %>%
   mutate_at(.vars = "Species", .funs = species_abbrev)
 
-fwrite(margo_r,"sp/lgm_margo_sp_r.csv")
-fwrite(margo_a,"sp/lgm_margo_sp_a.csv")
+fwrite(margo_r, "sp/lgm_margo_sp_r.csv")
+fwrite(margo_a, "sp/lgm_margo_sp_a.csv")
 ## -----------------------------------
 ## aggregate into functional groups
 ## -----------------------------------
 
-local_group_and_aggregate <- function(data){
+local_group_and_aggregate <- function(data) {
+  symbiosis_short_tbl <- symbiosis_tbl %>%
+    select(!c(Species)) %>%
+    distinct()
+  data_merged <- merge(data, symbiosis_short_tbl, by.x = "Species", by.y = "short_name") %>% select(!"Species")
 
-    symbiosis_short_tbl <- symbiosis_tbl %>% select(!c(Species)) %>% distinct()
-    data_merged <- merge(data, symbiosis_short_tbl, by.x="Species",by.y="short_name") %>% select(!"Species")
+  data_merged <- data_merged %>% remove_columns(c(
+    "sedimentation rate (cm/ky)",
+    "added by (name)", "Species", "short_name",
+    "calendar age estimate (cal ky BP)",
+    "Chronostratigraphic quality (chronozone level)",
+    "Laboratory", "Sample depth - lower (m)",
+    "date of addition", "Publication"
+  ))
 
-    data_merged <- data_merged %>% remove_columns(c("sedimentation rate (cm/ky)",
-                                      "added by (name)", "Species","short_name",
-                                      "calendar age estimate (cal ky BP)",
-                                      "Chronostratigraphic quality (chronozone level)",
-                                      "Laboratory", "Sample depth - lower (m)",
-                                      "date of addition","Publication"))
+  # allow different `Depth [m]` in one core sample to exist
+  data_merged <- data_merged %>%
+    group_by(
+      Core, `Coring device`, Latitude, Longitude,
+      `Water depth (m)`, Ocean, `Sample depth - upper (m)`,
+      Symbiosis, Spine
+    ) %>%
+    summarise_all(.funs = sum, na.rm = T) %>%
+    ungroup()
 
-    # allow different `Depth [m]` in one core sample to exist        
-    data_merged <- data_merged %>% group_by(Core, `Coring device`, Latitude, Longitude,
-                              `Water depth (m)`, Ocean, `Sample depth - upper (m)`,
-                              Symbiosis, Spine) %>% 
-      summarise_all(.funs = sum, na.rm=T) %>% ungroup()
-    
-    return(data_merged)
+  return(data_merged)
 }
 
-## covert them to long format, assign symbiosis and spine trait 
-margo_r %>% local_group_and_aggregate() %>% dplyr::filter(`Relative Abundance`<1.1) %>% 
-  fwrite( "fg/lgm_margo_fg_r.csv")
-margo_a %>% local_group_and_aggregate() %>% fwrite( "fg/lgm_margo_fg_a.csv")
+## covert them to long format, assign symbiosis and spine trait
+margo_r %>%
+  local_group_and_aggregate() %>%
+  dplyr::filter(`Relative Abundance` < 1.1) %>%
+  fwrite("fg/lgm_margo_fg_r.csv")
+margo_a %>%
+  local_group_and_aggregate() %>%
+  fwrite("fg/lgm_margo_fg_a.csv")
