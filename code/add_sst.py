22d21543af55ed26aea3dd7fa94de0ed2f449311
Rui Ying (Mac) <38958822+ruiying-ocean@users.noreply.github.com>
2023-11-21 13:12:40 +0000

    this commit uses original sst data

--- code/add_sst.py
+++ code/add_sst.py
@@ -119,36 +119,36 @@ def main():
     df_lat = 'Latitude'
     df_sst = 'SST'
 
-    # tierney_data = xr.open_dataset("tidy/Tierney2020_DA_ocn_regrid.nc")['SSTLGM']
-    # SST_LGM = DataProduct(tierney_data, lon='lon', lat='lat', lon_eastern=True)
-    # SST_LGM.match_dataframe(lgm_fg_a, df_lat, df_lon, df_sst).to_csv("tidy/lgm_fg_a_wsst.csv", index=False)
-    # SST_LGM.match_dataframe(lgm_fg_r, df_lat, df_lon, df_sst).to_csv("tidy/lgm_fg_r_wsst.csv", index=False)
-    # SST_LGM.match_dataframe(lgm_sp_a, df_lat, df_lon, df_sst).to_csv("tidy/lgm_sp_a_wsst.csv", index=False)
-    # SST_LGM.match_dataframe(lgm_sp_r, df_lat, df_lon, df_sst).to_csv("tidy/lgm_sp_r_wsst.csv", index=False)
+    tierney_data = xr.open_dataset("tidy/Tierney2020_DA_ocn_regrid.nc")['SSTLGM']
+    SST_LGM = DataProduct(tierney_data, lon='lon', lat='lat', lon_eastern=True)
+    SST_LGM.match_dataframe(lgm_fg_a, df_lat, df_lon, df_sst).to_csv("tidy/lgm_fg_a_wsst.csv", index=False)
+    SST_LGM.match_dataframe(lgm_fg_r, df_lat, df_lon, df_sst).to_csv("tidy/lgm_fg_r_wsst.csv", index=False)
+    SST_LGM.match_dataframe(lgm_sp_a, df_lat, df_lon, df_sst).to_csv("tidy/lgm_sp_a_wsst.csv", index=False)
+    SST_LGM.match_dataframe(lgm_sp_r, df_lat, df_lon, df_sst).to_csv("tidy/lgm_sp_r_wsst.csv", index=False)
 
     ## if add the annan sst, use the following code
     ## the SST is built from PI + LGM anomaly, and PI field uses ERSSTv5
-    annan_sst = xr.open_dataset("tidy/Annan2022_sst_field.nc")['sst']
-    SST_LGM_ALT = DataProduct(annan_sst, lon='longitude', lat='latitude', lon_eastern=False)
+    # annan_sst = xr.open_dataset("tidy/Annan2022_sst_field.nc")['sst']
+    # SST_LGM_ALT = DataProduct(annan_sst, lon='longitude', lat='latitude', lon_eastern=False)
     
-    SST_LGM_ALT.match_dataframe(lgm_fg_a, df_lat, df_lon, df_sst).to_csv("tidy/lgm_fg_a_wsst.csv", index=False)
-    SST_LGM_ALT.match_dataframe(lgm_fg_r, df_lat, df_lon, df_sst).to_csv("tidy/lgm_fg_r_wsst.csv", index=False)
-    SST_LGM_ALT.match_dataframe(lgm_sp_a, df_lat, df_lon, df_sst).to_csv("tidy/lgm_sp_a_wsst.csv", index=False)
-    SST_LGM_ALT.match_dataframe(lgm_sp_r, df_lat, df_lon, df_sst).to_csv("tidy/lgm_sp_r_wsst.csv", index=False)
+    # SST_LGM_ALT.match_dataframe(lgm_fg_a, df_lat, df_lon, df_sst).to_csv("tidy/lgm_fg_a_wsst.csv", index=False)
+    # SST_LGM_ALT.match_dataframe(lgm_fg_r, df_lat, df_lon, df_sst).to_csv("tidy/lgm_fg_r_wsst.csv", index=False)
+    # SST_LGM_ALT.match_dataframe(lgm_sp_a, df_lat, df_lon, df_sst).to_csv("tidy/lgm_sp_a_wsst.csv", index=False)
+    # SST_LGM_ALT.match_dataframe(lgm_sp_r, df_lat, df_lon, df_sst).to_csv("tidy/lgm_sp_r_wsst.csv", index=False)
     
-    # hadi_sst = xr.open_dataset("tidy/HadISST_PI.nc")['sst']
-    # PI_SST = DataProduct(hadi_sst, lon='longitude', lat='latitude', lon_eastern=False)
-    # PI_SST.match_dataframe(pi_fg_a, df_lat, df_lon, df_sst).to_csv("tidy/forcens_fg_a_wsst.csv", index=False)
-    # PI_SST.match_dataframe(pi_fg_r, df_lat, df_lon, df_sst).to_csv("tidy/forcens_fg_r_wsst.csv", index=False)
-    # PI_SST.match_dataframe(pi_sp_a, df_lat, df_lon, df_sst).to_csv("tidy/forcens_sp_a_wsst.csv", index=False)
-    # PI_SST.match_dataframe(pi_sp_r, df_lat, df_lon, df_sst).to_csv("tidy/forcens_sp_r_wsst.csv", index=False)
+    hadi_sst = xr.open_dataset("tidy/HadISST_PI.nc")['sst']
+    PI_SST = DataProduct(hadi_sst, lon='longitude', lat='latitude', lon_eastern=False)
+    PI_SST.match_dataframe(pi_fg_a, df_lat, df_lon, df_sst).to_csv("tidy/forcens_fg_a_wsst.csv", index=False)
+    PI_SST.match_dataframe(pi_fg_r, df_lat, df_lon, df_sst).to_csv("tidy/forcens_fg_r_wsst.csv", index=False)
+    PI_SST.match_dataframe(pi_sp_a, df_lat, df_lon, df_sst).to_csv("tidy/forcens_sp_a_wsst.csv", index=False)
+    PI_SST.match_dataframe(pi_sp_r, df_lat, df_lon, df_sst).to_csv("tidy/forcens_sp_r_wsst.csv", index=False)
 
-    ersst = xr.open_dataset("tidy/ersst_1854_1883.nc")['tos']
-    PI_SST_ALT = DataProduct(ersst, lon='lon', lat='lat', lon_eastern=False)
-    PI_SST_ALT.match_dataframe(pi_fg_a, df_lat, df_lon, df_sst).to_csv("tidy/forcens_fg_a_wsst.csv", index=False)
-    PI_SST_ALT.match_dataframe(pi_fg_r, df_lat, df_lon, df_sst).to_csv("tidy/forcens_fg_r_wsst.csv", index=False)
-    PI_SST_ALT.match_dataframe(pi_sp_a, df_lat, df_lon, df_sst).to_csv("tidy/forcens_sp_a_wsst.csv", index=False)
-    PI_SST_ALT.match_dataframe(pi_sp_r, df_lat, df_lon, df_sst).to_csv("tidy/forcens_sp_r_wsst.csv", index=False)
+    # ersst = xr.open_dataset("tidy/ersst_1854_1883.nc")['tos']
+    # PI_SST_ALT = DataProduct(ersst, lon='lon', lat='lat', lon_eastern=False)
+    # PI_SST_ALT.match_dataframe(pi_fg_a, df_lat, df_lon, df_sst).to_csv("tidy/forcens_fg_a_wsst.csv", index=False)
+    # PI_SST_ALT.match_dataframe(pi_fg_r, df_lat, df_lon, df_sst).to_csv("tidy/forcens_fg_r_wsst.csv", index=False)
+    # PI_SST_ALT.match_dataframe(pi_sp_a, df_lat, df_lon, df_sst).to_csv("tidy/forcens_sp_a_wsst.csv", index=False)
+    # PI_SST_ALT.match_dataframe(pi_sp_r, df_lat, df_lon, df_sst).to_csv("tidy/forcens_sp_r_wsst.csv", index=False)
     
     print(">>> [DONE] SST Added")
 
